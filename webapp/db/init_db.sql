91bf3bd27fce5c51eb037baa0979a613f7886c27
Jeremy Flusin <jeremy.flusin@itkweb.com>
2015-05-05 11:37:04 +0200

    init db

--- webapp/db/init_db.sql
+++ webapp/db/init_db.sql
@@ -1,9 +1,55 @@
-INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Vue');
-INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Odorat');
-INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Voix');
-INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Ouïe');
+INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Danse');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (1, 'Moonwalk');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (1, 'Lac des Cygnes');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (1, 'Booty shake');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (1, 'French cancan');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (1, 'Macarena');
 
-INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (1, 'Boule de feu');
-INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (1, 'Boule de glace');
+INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Sport');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (2, 'Footballeur');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (2, 'Skate-board');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (2, 'Arts martiaux');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (2, 'Houla Hop');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (2, 'Skieur');
+
+INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Langage');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (3, 'Accent portugais');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (3, 'Accent allemand');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (3, 'Accent russe');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (3, 'Parler chinois');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (3, 'Ne peut pas parler');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (3, 'Langue des signes');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (3, 'Répéter l\'hypnotiseur');
+
+INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Animaux');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (4, 'Devenir un chat');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (4, 'Devenir une grenouille');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (4, 'Devenir un serpent');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (4, 'Parler canard');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (4, 'Parler chèvre');
+
+INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Insolite');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Enfant de 4 ans');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Devenir Gollum');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Rire de sorcière');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Jeter des sushis');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Planète nudiste');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Fumer un joint/bedo');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Devenir un Super héro');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (5, 'Changement de sexe');
+
+INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Métiers');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (6, 'Militaire');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (6, 'Chef d\'orchestre');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (6, 'Hypnotiseur');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (6, 'Docteur');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (6, 'Coiffeur');
+
+INSERT INTO `hxvote`.`Category` (`label`) VALUES ('Illusions');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (7, 'Voir des boules de feu');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (7, 'Avoir une blessure');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (7, 'Voir des mouches partout');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (7, 'Papiers en billets');
+INSERT INTO `hxvote`.`Action` (`category_id`, `label`) VALUES (7, 'Amis imaginaires');
 
 INSERT INTO `hxvote`.`Administration` (`frontAccessible`) VALUES (1);
